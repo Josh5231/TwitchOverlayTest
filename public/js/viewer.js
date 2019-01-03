5655f8264dbea9e707317053303286df13a90b7c
Josh <josh5231@gmail.com>
2019-01-03 14:32:25 -0500

    Button Test
    Testing ability to interact with overlay via twich stream

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,5 +1,15 @@
 console.log("JS file loaded...");
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+ctx.beginPath();
 ctx.fillStyle = "#F00";
 ctx.fillRect(5, 5, 90, 90);
+ctx.closePath();
+
+function testClick(e){
+  console.log("Clicked button!");
+  ctx.fillStyle = "#00F";
+  ctx.beginPath();
+  ctx.fillRect(10, 10, 80, 80);
+  ctx.closePath();
+}
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var express = require("express");
 var app = express();
+//var session = require("express-session");
+//var bodyParser = require('body-parser');
 var port = process.env.PORT || 8080;
 var log = [];
 
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+// app.use(session({
+//     secret: "testSecret",//process.env.SES_SECRET,
+//     resave: false,
+//     saveUninitialized: true
+// }));
 
 app.use(function logIt(req, res, next) {
-  console.log("New incoming request!");
+  //console.log("New incoming request!");
  var d = new Date();
- log.push( { ip:req.ip, path:req.path, time:d.toTimeString() } );
+ log.push( { ip:req.ip, path:req.path, time:d.toTimeString(), } );
  if(log.length>20){ log.shift(); } //limit log.length to 20
  next();
 });
